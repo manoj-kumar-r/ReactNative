03d0869abc55a53575768fd89c89bcb68e355e58
Manoj Kumar R <manoj.kumar@sysfore.com>
2021-06-08 13:15:26 +0530

    Added Functional component

--- reactapp/src/component/Ninjas.js
+++ reactapp/src/component/Ninjas.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import NinjaItem from '../adapter/NinjaItem';
 
-class Ninjas extends Component {
-    render() {
-        console.log(this.props);
-        const { ninjas } = this.props
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <NinjaItem ninja={ninja} />
-            );
-        });
-        return ( ninjaList);
-    }
+//Functional Component 
+const Ninjas = (props) => {
+    console.log(props);
+    const { ninjas } = props
+    const ninjaList = ninjas.map(ninja => {
+        return (
+            <NinjaItem ninja={ninja} />
+        );
+    });
+    return ( ninjaList);
 }
+
+//Class Component
+// class Ninjas extends Component {
+//     render() {
+//         console.log(this.props);
+//         const { ninjas } = this.props
+//         const ninjaList = ninjas.map(ninja => {
+//             return (
+//                 <NinjaItem ninja={ninja} />
+//             );
+//         });
+//         return ( ninjaList);
+//     }
+// }
  
 export default Ninjas;
