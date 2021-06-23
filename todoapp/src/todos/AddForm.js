c6e7f4c2e1305f205a055a1cb7486549b404406a
Manoj Kumar R <manoj.kumar@sysfore.com>
2021-06-23 10:52:19 +0530

    Todo Completed

--- todoapp/src/todos/AddForm.js
+++ todoapp/src/todos/AddForm.js
@@ -15,6 +15,9 @@ class AddTodo extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addToDo(this.state);
+        this.setState({
+            content: ""
+        })
     }
 
     render() {
@@ -22,7 +25,7 @@ class AddTodo extends Component {
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label> Add New Todo</label>
-                    <input type="text" onChange={this.handleChange} />
+                    <input type="text" onChange={this.handleChange} value={this.state.content} />
                     <button onClick={this.handleSubmit}>Add</button>
                 </form>
             </div>
