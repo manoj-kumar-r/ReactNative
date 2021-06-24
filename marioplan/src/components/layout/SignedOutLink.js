7962316207b3ba822cc9fdc8f33f964b42539c1f
Manoj Kumar R <manoj.kumar@sysfore.com>
2021-06-24 12:13:13 +0530

    Basic Set up

---
+++ marioplan/src/components/layout/SignedOutLink.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SignedOutLinks extends Component {
+    render() {
+        return (
+            <div className="container">
+
+            </div>
+        );
+    }
+}
