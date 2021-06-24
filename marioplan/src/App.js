25ff189bcce7221d056d9f7953ed20a9400871d5
Manoj Kumar R <manoj.kumar@sysfore.com>
2021-06-24 12:10:03 +0530

    Added Route & Redux

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Mario Plan</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>Mario Plan</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
