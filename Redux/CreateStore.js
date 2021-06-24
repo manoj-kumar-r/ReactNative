b4133925a771def04321f83ab1661c03a0fcd5bb
Manoj Kumar R <manoj.kumar@sysfore.com>
2021-06-24 10:56:41 +0530

    Added Data to store & subscribe to listener

--- Redux/CreateStore.js
+++ Redux/CreateStore.js
@@ -11,19 +11,36 @@ function myReducer(state = initState ,action){
     if(action.type === 'ADD_TODO'){
       console.log("Action ADD")
       return {
-        todos: [...state.todos,action.todo]
+        ...state,
+        todos: [...state.todos,action.todo] 
       }
     }
+  if(action.type === 'ADD_POST'){
+    console.log("Action ADD")
+      return {
+        ...state,
+        posts: [...state.posts,action.post] 
+      }
+  }
 }
 
 //create a store 
 const store = createStore(myReducer);
 
+store.subscribe(() => {
+  console.log('State Updated');
+  console.log(store.getState());
+});
+
 //create a action 
-const toDoAction = {
-    type: 'ADD_TODO',
-    todo: 'Buy Milk'
-}
+const toDoAction = { type: 'ADD_TODO', todo: 'Buy Milk'}
 
 //pass action of the do 
 store.dispatch(toDoAction);
+store.dispatch({ type: 'ADD_TODO', todo: 'Sleep More'});
+store.dispatch({ type: 'ADD_TODO', todo: 'Wake Up'});
+store.dispatch({ type: 'ADD_POST', post: 'Posted New Comment'});
+store.dispatch({ type: 'ADD_POST', post: 'Posted New Comment 2'});
+
+
+
